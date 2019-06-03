276eb73aff4f7cb66d040cfd56768f45d5283ef3
Catharina Mesquita <anacatharina@infoway-pi.com.br>
2019-06-03 19:16:21 -0300

    Inicio do projeto de dorpbox clone

---
+++ app/public/src/controller/DropBoxController.js
@@ -0,0 +1,13 @@
+class DropBoxController{
+
+    constructor(){
+        this.btnSendFileEl = document.querySelector('#btn-send-file')
+        this.initEvents();
+    }
+
+    initEvents(){
+        this.btnSendFileEl.addEventListener('click', e => {
+            
+        })
+    }
+}
